308c4fa43a194ba481a668446fd4c6094bcf4a1b
aghp81 <aghp81@gmail.com>
2024-12-27 10:45:44 +0330

    فایل های استاتیک سی اس اس و جی اس در پوشه public قرار گیرند

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.json("AAA");
